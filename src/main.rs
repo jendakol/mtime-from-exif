0191a54cb7af1b2630694aa86049ee98786216ec
kolena <kolena@avast.com>
2019-11-18 23:12:25 +0100

    Some minor error handling

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,36 @@ use walkdir::{DirEntry, WalkDir};
 fn handle_file(de: DirEntry) -> () {
     let re = Regex::new(r"(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})").unwrap();
 
-    let exif = rexif::parse_file(de.path()).unwrap();
+    match rexif::parse_file(de.path()) {
+        Ok(exif) => {
+            match exif.entries.into_iter()
+                .find(|e| e.tag == ExifTag::DateTimeOriginal) {
+                Some(entry) => {
+                    let entry = entry.value.to_string();
+                    let caps = re.captures(entry.as_ref()).unwrap();
 
-    let entry = exif.entries.into_iter()
-        .find(|e| e.tag == ExifTag::DateTimeOriginal).unwrap()
-        .value.to_string();
+                    let mtime = format!(
+                        "{}-{}-{} {}:{}:{}",
+                        caps.get(1).unwrap().as_str(),
+                        caps.get(2).unwrap().as_str(),
+                        caps.get(3).unwrap().as_str(),
+                        caps.get(4).unwrap().as_str(),
+                        caps.get(5).unwrap().as_str(),
+                        caps.get(6).unwrap().as_str()
+                    );
 
-    let caps = re.captures(entry.as_ref()).unwrap();
+                    let mtime: SystemTime = mtime.parse::<humantime::Timestamp>().unwrap().into();
+                    let mtime: FileTime = FileTime::from(mtime);
 
-    let mtime = format!(
-        "{}-{}-{} {}:{}:{}",
-        caps.get(1).unwrap().as_str(),
-        caps.get(2).unwrap().as_str(),
-        caps.get(3).unwrap().as_str(),
-        caps.get(4).unwrap().as_str(),
-        caps.get(5).unwrap().as_str(),
-        caps.get(6).unwrap().as_str()
-    );
+                    println!("{:?} ({:?}) -> {:?}", de.path(), FileTime::from_last_modification_time(&std::fs::metadata(de.path()).unwrap()).unix_seconds(), mtime.unix_seconds());
 
-    let mtime: SystemTime = mtime.parse::<humantime::Timestamp>().unwrap().into();
-    let mtime: FileTime = FileTime::from(mtime);
-
-    println!("{:?} ({:?}) -> {:?}", de.path(), FileTime::from_last_modification_time(&std::fs::metadata(de.path()).unwrap()).unix_seconds(), mtime.unix_seconds());
-
-    filetime::set_file_mtime(de.path(), mtime).unwrap();
+                    filetime::set_file_mtime(de.path(), mtime).unwrap();
+                },
+                None => eprintln!("Could not find DateTimeOriginal tag in EXIF of {:?}", de.path())
+            }
+        },
+        Err(e) => eprintln!("Could not read EXIF for {:?}: {}", de.path(), e)
+    }
 }
 
 fn main() {
